735ad49bd09ca5eaca27b20b044a957231b9ea3b
Derek Somerville <47667752+DerekSomerville@users.noreply.github.com>
2021-07-08 10:28:20 +0100

    Fix working directory

--- src/ReadCSVFile.py
+++ src/ReadCSVFile.py
@@ -1,7 +1,14 @@
-import csv
+import csv, os
 class ReadCSVFile:
 
+    def fixWorkingDirectory(self):
+        currentWorkingDirectory = os.getcwd()
+        while "test" in currentWorkingDirectory or "src" in currentWorkingDirectory:
+            os.chdir("../")
+            currentWorkingDirectory = os.getcwd()
+
     def getFileData(self,fileName):
+        self.fixWorkingDirectory()
         fileData = []
         with open("resource/" + fileName, 'rt')as dataFile:
             fileReader = csv.reader(dataFile)
